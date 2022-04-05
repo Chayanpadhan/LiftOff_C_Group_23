88f9aa1ecbc7a396ab5c6811c96d8d8c792af25f
Chayanpadhan <99554336+Chayanpadhan@users.noreply.github.com>
2022-04-05 23:11:13 +0530

    Update assignment6.c

--- WEEK2/assignment6.c
+++ WEEK2/assignment6.c
@@ -3,20 +3,14 @@
 int main()
 {
     int i, j, N;
-
-    /* Input number of rows from user */
     printf("Enter number of rows: ");
     scanf("%d", &N);
-
-    /* Iterate over each row */
     for(i=1; i<=N; i++)
     {
-        /* Iterate over each column */
         for(j=1; j<=N; j++)
         {
             if(i==1 || i==N || j==1 || j==N)
             {
-                /* Print star for 1st, Nth row and column */
                 printf("*");
             }
             else
@@ -24,8 +18,6 @@ int main()
                 printf(" ");
             }
         }
-
-        /* Move to the next line/row */
         printf("\n");
     }
 
